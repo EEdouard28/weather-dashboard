faccca3e08833dd86eff462b10b0e66190d0e758
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-10-09 20:21:53 -0400

    header html and css added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 var apiKey = "d235a5db7da2633ff6e09047c8261443"; 
+
+
+
+
+https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={d235a5db7da2633ff6e09047c8261443}
+
+
+
+
+
+http://api.openweathermap.org/geo/1.0/direct?q={Hackensack},{NJ},{+1}&limit={limit}&appid={"d235a5db7da2633ff6e09047c8261443" 
+};
+
+
+40.71886639222204, -74.04092396598931
+
+
+https://api.openweathermap.org/data/3.0/onecall?lat={40.71886639222204}&lon={-74.04092396598931}&exclude={part}&appid={
+d235a5db7da2633ff6e09047c8261443}
+
+api.openweathermap.org/data/2.5/forecast?lat={40.71886639222204}&lon={-74.04092396598931}&appid={d235a5db7da2633ff6e09047c8261443}
