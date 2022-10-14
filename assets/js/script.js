47eef7b71f2f84e650e202af7425e8858e971a97
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-10-13 23:39:10 -0400

    final commits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,84 @@
-//authorization to grab weather
-var apiKey = "d235a5db7da2633ff6e09047c8261443";
+//DEPENDENCIES
 
-// targeting search button
-$(".searchBtn").on("click", function () {
-    var inputValue = $(".search-bar").val() // takes information from search bar when search button is click
-    console.log(inputValue)
-    geoCode(inputValue) // 
+//Weather API Key
+var APIKey = "d235a5db7da2633ff6e09047c8261443";
+
+//VARIABLES for search bar
+var searchEl = document.getElementById("searchBtn");
+var locationEl = document.getElementById("enter-location");
+var cityEl = document.getElementById("city-name");
+
+//VARIABLES to retrieve current weather & forecasy
+var todaysTempEl = document.getElementById("temperature");
+var todaysHumidityEl = document.getElementById("humidity");
+var todaysWindEl = document.getElementById("wind-speed");
+var todaysweatherEl = document.getElementById("current-weather");
+var forecastEl = document.getElementById("fiveday-forecast");
+
+
+
+//Function to retrieve current weather
+function getWeather(cityName) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    axios.get(queryURL)
+        .then(function (response) {
+            todaysweatherEl.classList.remove("d-none");
+
+            //Current weather date
+            var currentDate = new Date(response.data.dt * 1000);
+            var day = currentDate.getDate();
+            var month = currentDate.getMonth() + 1;
+            var year = currentDate.getFullYear();
+            cityEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+
+            //current weather data in data of HTML API
+            todaysTempEl.innerHTML = "Temperature: " + response.data.main.temp + " &#176F";
+            todaysHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+            todaysWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+
+            // Function to retrieve Weather Forecast In Area
+            var cityID = response.data.id;
+            var forecastWeather = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+            axios.get(forecastWeather)
+                .then(function (response) {
+                    forecastEl.classList.remove("d-none");
+
+                    var forecastEls = document.querySelectorAll(".fiveday");
+                    for (i = 0; i < forecastEls.length; i++) {
+                        forecastEls[i].innerHTML = "";
+                        var forecastIndex = i * 8 + 4;
+                        var forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
+                        var forecastDay = forecastDate.getDate();
+                        var forecastMonth = forecastDate.getMonth() + 1;
+                        var forecastYear = forecastDate.getFullYear();
+                        var forecastDateEl = document.createElement("p");
+
+                        forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+                        forecastEls[i].append(forecastDateEl);
+
+                        //Providing 5 day forecast information in containers
+                        var forecastWeatherEl = document.createElement("img");
+                        forecastEls[i].append(forecastWeatherEl);
+
+                        var forecastTempEl = document.createElement("p");
+                        forecastTempEl.innerHTML = "Temp: " + (response.data.list[forecastIndex].main.temp) + " &#176F";
+                        forecastEls[i].append(forecastTempEl);
+
+                        var forecastHumidityEl = document.createElement("p");
+                        forecastHumidityEl.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
+                        forecastEls[i].append(forecastHumidityEl);
+                    }
+                })
+        });
+}
+
+
+//search button listener
+searchEl.addEventListener("click", function () {
+    var searchTerm = locationEl.value;
+    getWeather(searchTerm);
+    searchHistory.push(searchTerm);
 })
 
-//call request from API, define data w/ queryURL, fetch info and 
-function geoCode(inputValue) {
-    var queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=5&appid=${apiKey}`
-    fetch(queryURL)
-        .then(response => response.json())
-        .then(data => {
-            // console.log(data)
-            searchWeather(data[0].lat, data[0].lon)
-            // forecastWeather(data[0].lat, data[0].lon)
-        })
-}
-
-// current weather
-function searchWeather(lat, lon) {
-    var queryURL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
-    fetch(queryURL)
-        .then(response => response.json())
-        .then(data => {
-            // console.log(data)
-            var temp = $("<h2>").text("temperature: " + data.main.temp)
-            var humid = $("<h2>").text("humidity: " + data.main.humidity)
-            var wind = $("<h2>").text("wind: " + data.wind.speed)
-
-            $("#currentWeather").append(temp, humid, wind)
-            forecastWeather(lat, lon)
-        })
-
-}
-
-async function forecastWeather(lat, lon) {
-    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
-    const response = await fetch(queryURL);
-    var data = await response.json();
-    var forecastData = [data.list[0].main.temp, data.list[1].main.humidity, data.list[2].speed.wind]
-    document.getElementById('weatherCard').innerHTML =
-        `
-    <p>Temperature: ${forecastData[0]}</p>
-    <p>Humidity: ${forecastData[1]}</p>
-    <p>Wind: ${forecastData[3]}</p>
-    `;
-}
-// function forecastWeather(lat, lon) {
-//     var queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
-//     fetch(queryURL)
-//         .then(response => response.json())
-//         .then(data => {
-
-//             for (var i = 4; i < data.list.length; i = i + 8) {
-//                 var card = $("<div>").addClass(".card").addClass(".list-group")
-//                 var ul = $("<ul>");
-//                 var li = $("<li>");
-//                 var date = $("<h3>").text("date: " + new Date(data.list[i].dt_txt))
-//                 var temp = $("<h3>").text("temp: " + data.list[i].main.temp)
-//                 var humid = $("<h3>").text("humidity: " + data.list[i].main.humidity)
-//                 var wind = $("<h3>").text("wind: " + data.list[i].wind.speed)
-
-//                 $("#forecastWeather").append(card)
-//                 card.append(date, temp, humid, wind)
-//                 console.log(data.list)
-//                 // console.log(data.list.length)
-//                 // card.append(ul)
-//                 // li.text("temp")
-//                 // $(".temp-item").append(temp)
-//             }
-//         })
-
-// }
 
 
-
-searchWeather();
-forecastWeather();
