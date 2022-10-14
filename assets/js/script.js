9caa024bd72a3a6166b55f54d5b69d9642e26fb0
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-10-13 20:42:00 -0400

    forecast weather js code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,51 +37,46 @@ function searchWeather(lat, lon) {
 
 }
 
-function forecastWeather(lat, lon) {
+async function forecastWeather(lat, lon) {
     var queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
-    fetch(queryURL)
-        .then(response => response.json())
-        .then(data => {
-            
-            for (var i = 4; i < data.list.length; i = i + 8) {
-                var card = $("<div>").addClass(".card").addClass(".list-group")
-                var ul = $("<ul>");
-                var li = $("<li>");
-                var date = $("<h3>").text("date: " + new Date(data.list[i].dt_txt))
-                var temp = $("<h3>").text("temp: " + data.list[i].main.temp)
-                var humid = $("<h3>").text("humidity: " + data.list[i].main.humidity)
-                var wind = $("<h3>").text("wind: " + data.list[i].wind.speed)
-
-                $("#forecastWeather").append(card)
-                card.append(date, temp, humid, wind)
-                console.log(data.list)
-                // console.log(data.list.length)
-                // card.append(ul)
-                // li.text("temp")
-                // $(".temp-item").append(temp)
-            }
-        })
-   
+    const response = await fetch(queryURL);
+    var data = await response.json();
+    var forecastData = [data.list[0].main.temp, data.list[1].main.humidity, data.list[2].speed.wind]
+    document.getElementById('weatherCard').innerHTML =
+        `
+    <p>Temperature: ${forecastData[0]}</p>
+    <p>Humidity: ${forecastData[1]}</p>
+    <p>Wind: ${forecastData[3]}</p>
+    `;
 }
+// function forecastWeather(lat, lon) {
+//     var queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+//     fetch(queryURL)
+//         .then(response => response.json())
+//         .then(data => {
 
+//             for (var i = 4; i < data.list.length; i = i + 8) {
+//                 var card = $("<div>").addClass(".card").addClass(".list-group")
+//                 var ul = $("<ul>");
+//                 var li = $("<li>");
+//                 var date = $("<h3>").text("date: " + new Date(data.list[i].dt_txt))
+//                 var temp = $("<h3>").text("temp: " + data.list[i].main.temp)
+//                 var humid = $("<h3>").text("humidity: " + data.list[i].main.humidity)
+//                 var wind = $("<h3>").text("wind: " + data.list[i].wind.speed)
 
-// figure out how to add dates
+//                 $("#forecastWeather").append(card)
+//                 card.append(date, temp, humid, wind)
+//                 console.log(data.list)
+//                 // console.log(data.list.length)
+//                 // card.append(ul)
+//                 // li.text("temp")
+//                 // $(".temp-item").append(temp)
+//             }
+//         })
 
-
-// var forecastTemp =
-  
-// retrieve the temperature
-
-//add temperature data inside li class temp-item
-// async function getWeather(weatherUrl) {
-//     const response = await fetch(weatherUrl);
-//     var data = await response.json();
-//     console.log(data);
-//     var temp = data.main.temp;
-//     var feelsLike = data.main.feels_like;
-//     var weatherDesc = data.weather[0].main;
-//     console.log(feelsLike);
-//     console.log(temp);
-//     console.log(weatherDesc)
-//     document.getElementById('weather').innerHTML = "Temperature: " + temp + " Feels Like: " + feelsLike + ", Conditions: " + weatherDesc;
 // }
+
+
+
+searchWeather();
+forecastWeather();
